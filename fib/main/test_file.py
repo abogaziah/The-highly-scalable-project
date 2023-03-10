064910c8b308b8cb49c0362e46b64274ad9fe103
abogaziah@gmail.com <abogaziah@gmail.com>
2023-03-11 00:09:11 +0200

    fixing auto test

--- fib/main/test_file.py
+++ fib/main/test_file.py
@@ -1,7 +1,8 @@
 from django.urls import reverse
 from django.test import Client
+import pytest
 
-
+@pytest.mark.django_db
 def test_main_view():
     client = Client()
     response = client.get(reverse('index') + '/?x=5', follow=True)
