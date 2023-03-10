9f7a720e34a3f51cc687602cec914ab1760a21ef
abogaziah@gmail.com <abogaziah@gmail.com>
2023-03-10 23:49:18 +0200

    integrating pytest and CI

---
+++ fib/main/test_file.py
@@ -0,0 +1,8 @@
+from django.urls import reverse
+from django.test import Client
+
+
+def test_main_view():
+    client = Client()
+    response = client.get(reverse('index') + '/?x=5', follow=True)
+    assert (int(response.content) == 5)
