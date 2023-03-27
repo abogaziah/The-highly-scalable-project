43285d78847c051e909e02ca764734e2fe6d9830
abogaziah@gmail.com <abogaziah@gmail.com>
2023-03-28 01:55:12 +0200

    added fib logic

--- fib/main/test_file.py
+++ fib/main/test_file.py
@@ -1,7 +1,8 @@
 from django.urls import reverse
 from django.test import Client
 
+
 def test_main_view():
     client = Client()
-    response = client.get(reverse('index') + '/?x=5', follow=True)
-    assert (int(response.content) == 5)
+    response = client.get(reverse("index") + "/?x=9", follow=True)
+    assert int(response.content) == 34
--- fib/main/views.py
+++ fib/main/views.py
@@ -2,5 +2,14 @@ from django.http import HttpResponse
 
 
 def index(request):
-    x= request.GET.get("x", "")
-    return HttpResponse(x)
+    x = request.GET.get("x", "")
+    return HttpResponse(fib(int(x)))
+
+
+def fib(num: int):
+    if not num:
+        return 0
+    if num == 1:
+        return 1
+
+    return fib(num - 1) + fib(num - 2)
